355b01622431fc675c0cb32bb9425a7af2e9401e
jjy <jjyruby@gmail.com>
2026-01-27 18:18:19 +0800

    Update vibe-caffeine to 0.1.3

--- Casks/vibe-caffeine.rb
+++ Casks/vibe-caffeine.rb
@@ -1,6 +1,6 @@
 cask "vibe-caffeine" do
-  version "0.1.2"
-  sha256 "d5a56094b76ee87ffa6626bc7d46ff2d4a5a9a2a7dcf93a78fc38419070f8863"
+  version "0.1.3"
+  sha256 "f4a52f7c4c8f406bdb8949dec742fc1bff971435a8ccdc18db8364297d96d19c"
 
   url "https://github.com/jjyr/vibe-caffeine/releases/download/v#{version}/VibeCaffeine.app.zip"
   name "Vibe Caffeine"
